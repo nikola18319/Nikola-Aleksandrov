277d8989d6ccfbf1d76b02be9fe242e44d3e3823
nikola18319 <57331296+nikola18319@users.noreply.github.com>
2022-01-12 23:32:44 +0200

    Add files via upload

---
+++ Java-Game-Brick-Breaker/src/Main.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame obj=new JFrame();
+		Gameplay gamePlay = new Gameplay();
+		
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("FastBall");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+                obj.setVisible(true);
+
+	}
+
+}
